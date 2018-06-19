5ea32e2e13d4d73a656e0e02df3bb7ecdfe36b0f
Rob Warner <rob.warner@availity.com>
2018-06-19 17:42:51 -0400

    Modernize the js a bit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var services = require('./players_grpc_pb')
 
 var grpc = require('grpc')
 
-proto.pb.Player.prototype.toString = function() {
-  return '[' + this.getId() + '] ' + this.getName() + ' (' + this.getPosition() + ') -- #' + this.getNumber() + ', ' + this.getHeight() + ', ' + this.getWeight() + 'lb, ' + this.getAge() + 'yo, ' + this.getExperience() + 'exp -- ' + this.getCollege()
+messages.Player.prototype.toString = function() {
+  return `[${this.getId()}] ${this.getName()} (${this.getPosition()}) -- #${this.getNumber()}, ${this.getHeight()}, ${this.getWeight()}lb, ${this.getAge()}yo, ${this.getExperience()}exp -- ${this.getCollege()}`
 }
 
 function main() {
@@ -18,12 +18,16 @@ function main() {
 function listPlayers(client, position) {
   var request = new messages.ListPlayersRequest()
   request.setPosition(position)
-  client.listPlayers(request, function(err, response) {
-    if (response.getErr() !== "") {
-      console.log(position + ' : ' + response.getErr())
+  client.listPlayers(request, (err, response) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    if (response.getErr()) {
+      console.log(`${position} : ${response.getErr()}`)
     } else {
-      response.getPlayersList().forEach(function(player) {
-        console.log(position + ' : ' + player.toString())
+      response.getPlayersList().forEach(player => {
+        console.log(`${position} : ${player.toString()}`)
       })
     }
   })
@@ -32,11 +36,15 @@ function listPlayers(client, position) {
 function getPlayer(client, id) {
   var request = new messages.GetPlayerRequest()
   request.setId(id)
-  client.getPlayer(request, function(err, response) {
-    if (response.getErr() !== "") {
-      console.log(id + ' : ' + response.getErr())
+  client.getPlayer(request, (err, response) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    if (response.getErr()) {
+      console.log(`${id} : ${response.getErr()}`)
     } else {
-      console.log(id + ' : ' + response.getPlayer().toString())
+      console.log(`${id} : ${response.getPlayer().toString()}`)
     }
   })
 }
